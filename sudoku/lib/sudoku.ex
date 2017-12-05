a89f2710ba094e7d38ce6197d717c8607e31f025
willHol <thenecroyeti@gmail.com>
2017-12-05 20:51:53 +1300

    oops

--- sudoku/lib/sudoku.ex
+++ sudoku/lib/sudoku.ex
@@ -15,15 +15,15 @@ defmodule Sudoku do
         # Could lead to correct solution
         {:cont, [x, y]} = Board.next_empty_elem(board, [x, y])
 
-           Board.solve_brute(Board.put(board, [x, y], 1), [x, y])
-        || Board.solve_brute(Board.put(board, [x, y], 2), [x, y])
-        || Board.solve_brute(Board.put(board, [x, y], 3), [x, y])
-        || Board.solve_brute(Board.put(board, [x, y], 4), [x, y])
-        || Board.solve_brute(Board.put(board, [x, y], 5), [x, y])
-        || Board.solve_brute(Board.put(board, [x, y], 6), [x, y])
-        || Board.solve_brute(Board.put(board, [x, y], 7), [x, y])
-        || Board.solve_brute(Board.put(board, [x, y], 8), [x, y])
-        || Board.solve_brute(Board.put(board, [x, y], 9), [x, y])
+           solve_brute(Board.put(board, [x, y], 1), [x, y])
+        || solve_brute(Board.put(board, [x, y], 2), [x, y])
+        || solve_brute(Board.put(board, [x, y], 3), [x, y])
+        || solve_brute(Board.put(board, [x, y], 4), [x, y])
+        || solve_brute(Board.put(board, [x, y], 5), [x, y])
+        || solve_brute(Board.put(board, [x, y], 6), [x, y])
+        || solve_brute(Board.put(board, [x, y], 7), [x, y])
+        || solve_brute(Board.put(board, [x, y], 8), [x, y])
+        || solve_brute(Board.put(board, [x, y], 9), [x, y])
       else
         false
       end
