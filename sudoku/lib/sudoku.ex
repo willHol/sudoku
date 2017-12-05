df2bdef330edb47039e73469b451ccd07dd87f87
willHol <thenecroyeti@gmail.com>
2017-12-05 20:50:32 +1300

    refactor

--- sudoku/lib/sudoku.ex
+++ sudoku/lib/sudoku.ex
@@ -1,18 +1,32 @@
 defmodule Sudoku do
   @moduledoc """
-  Documentation for Sudoku.
+  
   """
 
-  @doc """
-  Hello world.
+  def solve_brute(board, [x, y] \\ [-1, 0]) do
+    if Board.full?(board) do
+      if Board.correct?(board) do
+        board
+      else
+        false
+      end
+    else
+      if Board.correct?(board) do
+        # Could lead to correct solution
+        {:cont, [x, y]} = Board.next_empty_elem(board, [x, y])
 
-  ## Examples
-
-      iex> Sudoku.hello
-      :world
-
-  """
-  def hello do
-    :world
+           Board.solve_brute(Board.put(board, [x, y], 1), [x, y])
+        || Board.solve_brute(Board.put(board, [x, y], 2), [x, y])
+        || Board.solve_brute(Board.put(board, [x, y], 3), [x, y])
+        || Board.solve_brute(Board.put(board, [x, y], 4), [x, y])
+        || Board.solve_brute(Board.put(board, [x, y], 5), [x, y])
+        || Board.solve_brute(Board.put(board, [x, y], 6), [x, y])
+        || Board.solve_brute(Board.put(board, [x, y], 7), [x, y])
+        || Board.solve_brute(Board.put(board, [x, y], 8), [x, y])
+        || Board.solve_brute(Board.put(board, [x, y], 9), [x, y])
+      else
+        false
+      end
+    end
   end
 end
--- sudoku/lib/sudoku/board.ex
+++ sudoku/lib/sudoku/board.ex
@@ -153,31 +153,4 @@ defmodule Sudoku.Board do
   def next_box(board, [x, y]) do
     next_elem(board, [x, y], @square_dim)
   end
-
-  def solve_brute(board, [x, y] \\ [-1, 0]) do
-    if full?(board) do
-      if correct?(board) do
-        board
-      else
-        false
-      end
-    else
-      if correct?(board) do
-        # Could lead to correct solution
-        {:cont, [x, y]} = next_empty_elem(board, [x, y])
-
-           solve_brute(put(board, [x, y], 1), [x, y])
-        || solve_brute(put(board, [x, y], 2), [x, y])
-        || solve_brute(put(board, [x, y], 3), [x, y])
-        || solve_brute(put(board, [x, y], 4), [x, y])
-        || solve_brute(put(board, [x, y], 5), [x, y])
-        || solve_brute(put(board, [x, y], 6), [x, y])
-        || solve_brute(put(board, [x, y], 7), [x, y])
-        || solve_brute(put(board, [x, y], 8), [x, y])
-        || solve_brute(put(board, [x, y], 9), [x, y])
-      else
-        false
-      end
-    end
-  end
 end
